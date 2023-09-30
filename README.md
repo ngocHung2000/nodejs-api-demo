6fd8f3547d4dfc10a8445354c292951b6fd956a0
ngochung1809 <ngochung1809@ngochung1809.ngochung1809>
2023-09-30 23:45:14 +0700

    update cmd test api

---
+++ README.md
@@ -0,0 +1,29 @@
+### Test API
+- GET
+```shell
+curl -X GET http://localhost:3000/tasks
+```
+
+- POST
+```shell
+curl -X POST http://localhost:3000/tasks \
+-H "Content-Type: application/json" \
+-d '{"task": "Deploy App", "status": "1"}'
+```
+
+- GET Item
+```shell
+curl -X GET http://localhost:3000/tasks/1
+```
+
+- PUT
+```shell
+curl -X PUT http://localhost:3000/tasks/1 \
+-H "Content-Type: application/json" \
+-d '{"task": "Deploy App Update", "status": "0"}'
+```
+
+- DELETE
+```shell
+curl -X DELETE http://localhost:3000/tasks/3
+```
--- init-db.sql
+++ init-db.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `tasks` (
- `id` int(11) NOT NULL,
+ `id` int(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
  `task` varchar(200) NOT NULL,
  `status` tinyint(1) NOT NULL DEFAULT '1',
  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `tasks` (`id`, `task`, `status`, `created_at`) VALUES
-(1, 'Find bugs', 1, '2019-04-10 23:50:40'),
-(2, 'Review code', 1, '2019-04-10 23:50:40'),
-(3, 'Fix bugs', 1, '2019-04-10 23:50:40'),
-(4, 'Refactor Code', 1, '2019-04-10 23:50:40'),
-(5, 'Push to prod', 1, '2019-04-10 23:50:50');
+INSERT INTO `tasks` (`task`, `status`, `created_at`) VALUES
+('Find bugs', 1, '2019-04-10 23:50:40'),
+('Review code', 1, '2019-04-10 23:50:40'),
+('Fix bugs', 1, '2019-04-10 23:50:40'),
+('Refactor Code', 1, '2019-04-10 23:50:40'),
+('Push to prod', 1, '2019-04-10 23:50:50');
